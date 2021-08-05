46879238797e1564beb29612ea1991eb95e38b31
Jeon Seongsoo <stome159@naver.com>
2021-08-05 18:42:02 +0900

    commit gitigonre file

---
+++ src/main/java/com/springboot/web/Dto/HelloResponseDto.java
@@ -0,0 +1,21 @@
+package com.springboot.web.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Getter
+@AllArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
