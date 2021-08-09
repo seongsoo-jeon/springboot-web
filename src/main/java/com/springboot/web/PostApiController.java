5ff70fc1729d9e334145ebb557e103c17f2be0cb
Jeon Seongsoo <stome159@naver.com>
2021-08-09 21:34:12 +0900

    API 개발

---
+++ src/main/java/com/springboot/web/PostApiController.java
@@ -0,0 +1,29 @@
+package com.springboot.web;
+
+import com.springboot.service.PostService;
+import com.springboot.web.Dto.PostsResponseDto;
+import com.springboot.web.Dto.PostsSaveRequestDto;
+import com.springboot.web.Dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postService.findById(id);
+    }
+}
